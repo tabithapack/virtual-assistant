0fa86673ed363c257a02ff81e729bdc4b32f18fb
Tabitha Pack <tabitha.pack.95@gmail.com>
2017-06-18 19:40:31 +0000

    Added navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,6 @@ class PagesController < ApplicationController
   def zuzeca
   end
   
+  def heyoka
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'zuzeca', to: 'pages#zuzeca'
+  get 'heyoka', to: 'pages#heyoka'
 end
